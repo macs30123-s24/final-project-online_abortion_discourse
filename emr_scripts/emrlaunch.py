021a9394673f4e8ea7d0e8f58991826361f02c08
ecg1331 <griffin13@uchicago.edu>
2024-05-23 21:05:35 -0500

    final

--- emr_scripts/emrlaunch.py
+++ emr_scripts/emrlaunch.py
@@ -4,21 +4,25 @@ aws emr create-cluster \
 --name "Spark NLP 5.3.3" \
 --release-label emr-6.2.0 \
 --applications Name=Hadoop Name=Spark Name=Hive Name=JupyterHub \
---instance-type m4.xlarge\
+--instance-type m4.xlarge \
 --instance-count 4 \
 --use-default-roles \
 --log-uri "s3://131313113finalproject/" \
 --bootstrap-actions Path="s3://131313113finalproject/setup.sh",Name=custom \
---configurations file://~/desktop/30123/project/emr_scripts/sparknlp-config.json \
---ec2-attributes '{"KeyName": "vockey2"}' --region us-east-1\
+--configurations file://~/desktop/30123/final-project-online_abortion_discourse/emr_scripts/sparknlp-config.json \
+--ec2-attributes '{"KeyName": "vockey2"}' \
+--region us-east-1
 '''
+
+# normally launched the cluster with anywhere from 3 - 5 m4.xlarge instances
 
 # after cluster is launched:
 # sudo pip3 install --upgrade pip
 # sudo python3 -m pip install pandas seaborn matplotlib wordcloud
 
 '''
-ssh -i ~/.aws/vockey2.pem -NL 9443:localhost:9443 hadoop@ec2-34-230-23-74.compute-1.amazonaws.com
+ssh -i ~/.aws/vockey2.pem hadoop@ec2-54-162-215-247.compute-1.amazonaws.com
+ssh -i ~/.aws/vockey2.pem -NL 9443:localhost:9443 hadoop@ec2-54-162-215-247.compute-1.amazonaws.com
 https://localhost:9443
 '''
 
